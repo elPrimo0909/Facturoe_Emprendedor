883bf62873d9b9b81c8b88ba834a808f4134bcc2
elprimo <elprimompv@gmail.com>
2024-06-27 17:03:48 -0500

    2706243

---
+++ facturoe/pos/header.php
@@ -0,0 +1,10 @@
+<header  >
+
+<img src="../pos/img/logo_010419.png" width="110" height="20">
+<a   href="../pos/" target="_self" class="linknavegacion"  > POS </a>
+
+
+<a   href="../inventario_f/" target="_self" class="linknavegacion"  > Inventario </a>
+
+
+</header>
